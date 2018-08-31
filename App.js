3b0c149b54eeea1cf4749a34a508524992eb647a
zapicojunredexter <zapicojunredexter@yahoo.com>
2018-08-31 13:24:45 +0800

    change test master 2

--- App.js
+++ App.js
@@ -21,7 +21,7 @@ export default class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
+        <Text style={styles.welcome}>changed test master 2 Welcome to React Native!</Text>
         <Text style={styles.instructions}>Hi World!</Text>
         <Text style={styles.instructions}>{instructions} this should work now again</Text>
       </View>
